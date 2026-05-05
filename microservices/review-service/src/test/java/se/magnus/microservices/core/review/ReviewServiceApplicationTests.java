9bf597f568991d7015b52741fe0e2f85e736af5c
Polici Claudiu <prettypolici@yahoo.com>
2026-05-05 06:57:47 +0300

    capitolul 12 = parteaa doua. ultimul actualizat este auth server. Mai verifica odata ca ai pus in build.gradle dependentele si application.yaml din config-repo si bootstrap.yml din auth server. apoi continua cu ce mai e in spring-cloud

--- microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
@@ -23,7 +23,8 @@ import se.magnus.microservices.core.review.persistence.ReviewRepository;
 @SpringBootTest(webEnvironment = RANDOM_PORT, properties = {
   "spring.cloud.stream.defaultBinder=rabbit",
   "logging.level.se.magnus=DEBUG",
-  "eureka.client.enabled=false"})
+  "eureka.client.enabled=false",
+  "spring.cloud.config.enabled=false"})
 class ReviewServiceApplicationTests extends MySqlTestBase {
 
   @Autowired
